1dc11fd820135f50cfd768d59366223021ead822
it22587206 <it22587206@my.sliit.lk>
2025-02-27 14:44:00 +0530

    signup backend

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,14 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 export default function SignUp() {
+  const [formData, setFormData] = useState({})
+  const handleChange = (e) => {
+      setFormData({
+        ...formData,
+        [e.target.id]: e.target.value,
+      });
+  };
+
+  const handleSubmit = async(e) => {
+    e.preventDefault();
+    const res = await fetch('/api/auth/signup', 
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application.json',
+        },
+        body: JSON.stringify(formData),
+      }
+    );
+     const data = await res.json();
+     console.log(data);
+  };
+
+  console.log(formData);
+
   return (
     <div className='p-3 max-w-xl mx-auto'>
       <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
-      <form className='flex flex-col gap-5'>
-        <input type="text" placeholder='Username' className='border p-3 rounded-lg' id='username'/>
-        <input type="text" placeholder='Email' className='border p-3 rounded-lg' id='email'/>
-        <input type="text" placeholder='Password' className='border p-3 rounded-lg' id='password'/>
+      <form onSubmit={handleSubmit} className='flex flex-col gap-5'>
+        <input type="text" placeholder='Username' className='border p-3 rounded-lg' id='username' onChange={handleChange}/>
+        <input type="text" placeholder='Email' className='border p-3 rounded-lg' id='email' onChange={handleChange}/>
+        <input type="Password" placeholder='Password' className='border p-3 rounded-lg' id='password' onChange={handleChange}/>
         <button className='bg-slate-500 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>Sign Up</button>
       </form>
       <div className='flex gap-2 mt-5'>
