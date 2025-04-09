33834ffb742394272153eba6812df02e9a702c05
it22587206 <it22587206@my.sliit.lk>
2025-04-09 23:58:38 +0530

    update the header and make the profile page privet

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import About from './pages/About';
 import Profile from './pages/Profile';
 import Home from './pages/Home';
 import Header from './components/Header';
+import PrivetRoute from './components/PrivetRoute';
 
 export default function App() {
   return (
@@ -16,7 +17,9 @@ export default function App() {
         <Route path='/sign-in' element={<Signin/>}/>
         <Route path='/sign-up' element={<SignUp/>}/>
         <Route path='/about' element={<About/>}/>
-        <Route path='/profile' element={<Profile/>}/>
+        <Route element= {<PrivetRoute />}>
+          <Route path='/profile' element={<Profile/>}/>
+        </Route>
       </Routes>
     </BrowserRouter>
   )
